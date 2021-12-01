b24a3d207e914bd32cb04e3428310ca92b86b7d6
Erik Karsten <erik@opcode.xyz>
2021-12-01 13:12:47 +0200

    Basic ant layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Sider>Sider</Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 }
