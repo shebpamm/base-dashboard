0067eeaf21948dd7bffdbbfe06197f9e696ddf57
Erik Karsten <erik@opcode.xyz>
2021-12-02 14:16:37 +0200

    Change from <List> to <Table>

--- src/stockComponent.js
+++ src/stockComponent.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React from 'react';
-import { List } from 'antd';
+import { Table , Avatar, Space} from 'antd';
+
+import StockInputComponent from './stockInputComponent';
 
 class stockComponent extends React.Component {
 
@@ -12,10 +14,58 @@ class stockComponent extends React.Component {
         };
     }
 
+    columns = [
+        {
+            title: 'Icon',
+            dataIndex: 'iconUrl',
+            key: 'icon',
+            render: url => (
+                <Avatar src={url} />
+            ),
+        },
+        {
+            title: 'Name',
+            dataIndex: 'displayName',
+            key: 'displayName',
+        },
+        {
+            title: 'Item ID',
+            dataIndex: 'itemId',
+            key: 'itemId',
+        },
+        {
+            title: 'Quantity',
+            dataIndex: 'minQuantity',
+            key: 'minQuantity',
+        },
+        {
+            title: 'Batch Size',
+            dataIndex: 'batchSize',
+            key: 'batchSize',
+        },
+        {
+            title: 'Actions',
+            key: 'action',
+            render: () => {
+            return (<Space size="middle">
+                <a>Edit</a>
+                <a>Delete</a>
+            </Space>
+            )},
+        }
+    ];
+
     fetchData() {
         this.setState({ ...this.state, isFetching: true })
         axios.get('https://mc-base.azurewebsites.net/api/stockItems?code=krAXisqkgHEWfgVkwkYAwHyumeE302koi80bw/tuaZXJ/f85DyMqaw==').then( req => {
-            this.setState({ ...this.state, isFetching: false, stock: req.data })
+            console.log(req.data);
+            const stockData = req.data.map( item => {
+                return { 
+                    ...item, 
+                    iconUrl: 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/fc/Iron_Ingot_JE3_BE2.png',
+                }
+            })
+            this.setState({ ...this.state, isFetching: false, stock: stockData })
         }) 
     }
 
@@ -26,19 +76,12 @@ class stockComponent extends React.Component {
     render() {
         return (
             <div>
-                <p>{this.state.isFetching ? 'Fetching users...' : ''}</p>
-                <List
-                    itemLayout="horizontal"
+                < StockInputComponent />
+                <Table
+                    columns={this.columns}
                     dataSource={ this.state.stock }
-                    renderItem={ item => (
-                        <List.Item>
-                            <List.Item.Meta 
-                                title={ item.displayName }
-                                description={ item.itemId }
-                            />
-                        </List.Item>
-                    )}
-                ></List>
+                    loading={ this.state.isFetching }
+                ></Table>
             </div>
         )
     }
