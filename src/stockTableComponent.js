385179d5c1d40afba38e4f26ec5ab471d3d72973
Erik Karsten <erik@opcode.xyz>
2021-12-28 16:59:36 +0200

    Made stockTable editable and added edit action

--- src/stockTableComponent.js
+++ src/stockTableComponent.js
@@ -1,69 +1,20 @@
 import axios from 'axios';
 import React from 'react';
-import { message, Table , Avatar, Space, Input, InputNumber, Form} from 'antd';
+import { message, Table , Avatar, Space, Input, InputNumber, Form, Typography, Popconfirm} from 'antd';
 
-import StockInputComponent from './stockInputComponent';
+class EditableCell extends React.Component {
 
-class StockTableComponent extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
-    }
-
-    columns = [
-        {
-            title: 'Icon',
-            dataIndex: 'itemId',
-            key: 'icon',
-            render: id => (
-                <Avatar src={`https://minecraftresources.blob.core.windows.net/icons/${encodeURIComponent(id)}.png`} shape='square' style={{ imageRendering: 'pixelated' }}/>
-            ),
-        },
-        {
-            title: 'Name',
-            dataIndex: 'displayName',
-            key: 'displayName',
-        },
-        {
-            title: 'Item ID',
-            dataIndex: 'itemId',
-            key: 'itemId',
-        },
-        {
-            title: 'Quantity',
-            dataIndex: 'minQuantity',
-            key: 'minQuantity',
-        },
-        {
-            title: 'Batch Size',
-            dataIndex: 'batchSize',
-            key: 'batchSize',
-        },
-        {
-            title: 'Actions',
-            key: 'action',
-            render: () => {
-            return (<Space size="middle">
-                <a>Edit</a>
-                <a>Delete</a>
-            </Space>
-            )},
-        }
-    ];
-
-    EditableCell = ({
-        editing,
-        dataIndex,
-        title,
-        inputType,
-        record,
-        index,
-        children,
-        ...restProps
-    }) => {
+    render() {
+        const {
+            editing,
+            dataIndex,
+            title,
+            inputType,
+            record,
+            index,
+            children,
+            ...restProps
+        } = this.props;
         const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
         return (
             <td {...restProps}>
@@ -87,21 +38,166 @@ class StockTableComponent extends React.Component {
                 )}
             </td>
         );
-    };
+        };
+    }
+class StockTableComponent extends React.Component {
+
+    formRef = React.createRef();
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            editingKey: '',
+        };
+    }
+
+    columns = [
+        {
+            title: 'Icon',
+            dataIndex: 'itemId',
+            key: 'icon',
+            render: id => (
+                <Avatar src={`https://minecraftresources.blob.core.windows.net/icons/${encodeURIComponent(id)}.png`} shape='square' style={{ imageRendering: 'pixelated' }}/>
+            ),
+        },
+        {
+            title: 'Name',
+            dataIndex: 'displayName',
+            key: 'displayName',
+            editable: true,
+            type: 'text',
+        },
+        {
+            title: 'Item ID',
+            dataIndex: 'itemId',
+            key: 'itemId',
+            editable: true,
+            type: 'text',
+        },
+        {
+            title: 'Quantity',
+            dataIndex: 'minQuantity',
+            key: 'minQuantity',
+            editable: true,
+            type: 'number',
+        },
+        {
+            title: 'Batch Size',
+            dataIndex: 'batchSize',
+            key: 'batchSize',
+            editable: true,
+            type: 'number',
+        },
+        {
+            title: 'Actions',
+            key: 'action',
+            render: (_, record) => {
+                const editable = this.isEditing(record);
+                return editable ? (
+                    <span>
+                        <Typography.Link
+                            onClick={() => this.save(record.itemId)}
+                            style={{
+                                marginRight: 8,
+                            }}
+                        >
+                            Save
+                        </Typography.Link>
+                        <Popconfirm title="Sure to cancel?" onConfirm={() => this.cancel()}>
+                            <a href="#">Cancel</a>
+                        </Popconfirm>
+                    </span>
+                ) : (
+                    <span>
+                        <Typography.Link
+                            disabled={this.state.editingKey !== ''}
+                            onClick={() => this.edit(record)}
+                            style={{
+                                marginRight: 8,
+                            }}
+                        >
+                            Edit
+                        </Typography.Link>
+                        <Popconfirm title="Sure to delete?" onConfirm={() => this.delete(record.itemId)}>
+                            <a href="#">Delete</a>
+                        </Popconfirm>
+                    </span>
+                );
+
+        },
+        }
+    ];
+
+    mergedColumns = this.columns.map((col) => {
+        if (!col.editable) {
+        return col;
+        }
+
+        return {
+        ...col,
+        onCell: (record) => ({
+            record,
+            inputType: col.type,
+            dataIndex: col.dataIndex,
+            title: col.title,
+            editing: this.isEditing(record),
+        }),
+        };
+    });
+
+    isEditing(record) {
+        return record.itemId === this.state.editingKey;
+    }
+
+    edit(record) {
+        this.formRef.current.setFieldsValue(record);
+        this.setState({
+            ...this.state,
+            editingKey: record.itemId,
+        });
+    }
+
+    cancel() {
+        this.setState({
+            ...this.state,
+            editingKey: '',
+        });
+    }
+
+    delete() {
+        
+    }
+
+    save = async (key, row) => {
+        try {
+            const row = await this.formRef.current.validateFields();
+            this.props.updateStockItem(row);
+            this.setState({
+                ...this.state,
+                editingKey: '',
+            });
+        } catch (errInfo) {
+            console.log('Validate Failed:', errInfo);
+        }
+    }
+
+    
 
     render() {
         return (
-            <Table
-                columns={this.columns}
-                dataSource={ this.props.data }
-                loading={ this.props.loading }
-                rowKey={record => record.itemId}
-                components={{
-                    body: {
-                        cell: this.EditableCell,
-                    },
-                }}
-            ></Table>
+            <Form ref={this.formRef} component={false}>
+                <Table
+                    columns={ this.mergedColumns }
+                    dataSource={ this.props.data }
+                    loading={ this.props.loading }
+                    rowKey={ record => record.itemId }
+                    components={{
+                        body: {
+                            cell: EditableCell,
+                        },
+                    }}
+                ></Table>
+            </Form>
         )
     }
 }
